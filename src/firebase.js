ec5a48f3dc2aa17ac7a991e3fc1474ace8f45155
Harshtih <harshitvarma638@gmail.com>
2024-03-25 09:55:08 +0530

    added .env to .gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 import{getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBxu5pkwvYYLpmgx4H2MjU_2AitTdxcKsY",
-    authDomain: "login-chess-f8ac9.firebaseapp.com",
-    projectId: "login-chess-f8ac9",
-    storageBucket: "login-chess-f8ac9.appspot.com",
-    messagingSenderId: "704322005920",
-    appId: "1:704322005920:web:d0596c1c4080e86da592c5",
-    measurementId: "G-B8R9G97J08"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
